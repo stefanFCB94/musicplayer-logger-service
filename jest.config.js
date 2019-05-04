d69807ea48d6f764e34756be7d2628e66c91d5f7
Stefan Läufle <stefan.laeufle@gmail.com>
2019-05-04 19:57:57 +0200

    Fixed code style

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-process.env.DB_HOST="localhost";
+process.env.DB_HOST='localhost';
 process.env.DB_PORT=5432;
-process.env.LOGGER_DB_USERNAME="logger";
-process.env.LOGGER_DB_PASSWORD="0JjV5(G26,^{a&L8";
-process.env.LOGGER_DB_DATABASE="logger";
+process.env.LOGGER_DB_USERNAME='logger';
+process.env.LOGGER_DB_PASSWORD='0JjV5(G26,^{a&L8';
+process.env.LOGGER_DB_DATABASE='logger';
 
 module.exports = {
   preset: 'ts-jest',
